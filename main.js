d977d938fc7d98c65f5783c527b8dfb9787ea010
Mike <mike@nvavia.ru>
2023-01-31 08:33:45 +0500

    clock 7 update

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-var fa = function (f) { console.log(f); return f(f); };
-var fn = function (x) { return x > 3 ? x : fn(x + 1); };
-console.log(fa(fn)(1));
+// type ft = (x:any) => ft ;
+// const fa = (f:ft) => { console.log(f) ; return f(f); } ;
+// const fn:ft = (x:any) => x > 3 ? x : fn ( x + 1) ;
+var compose = function (f, g) { return function (p) { return g(f(p)); }; };
+var compose2 = function (f, g) { return function () {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i] = arguments[_i];
+    }
+    return g(f(args));
+}; };
+var f1 = function (x) { return x + "f1!"; };
+var f2 = function (x) { return x + "f2!"; };
+console.log(compose(f1, f2)(2));
+console.log(compose2(compose2(f1, f2), f1)(2));
+//console.log( fa( fn )(1) )
--- main.ts
+++ main.ts
@@ -1,8 +1,34 @@
-type ft = (x:any) => ft ;
-
-const fa = (f:ft) => { console.log(f) ; return f(f); } ;
-
-const fn:ft = (x:any) => x > 3 ? x : fn ( x + 1) ;
+// type ft = (x:any) => ft ;
+// const fa = (f:ft) => { console.log(f) ; return f(f); } ;
+// const fn:ft = (x:any) => x > 3 ? x : fn ( x + 1) ;
 
 
-console.log( fa( fn )(1) )
+type fnTT<T>  = (...args:any[]) => T 
+type fnFT<T>  = ( arg:T ) => any 
+
+type fnT<T,U> = (arg:T) => U 
+
+
+
+const compose = <T,U,V>( f:fnT<T,U> , g:fnT<U,V> )  => (p:T) => g(f(p))
+
+const compose2 = <U>( f:fnTT<U> , g:fnFT<U> )  => (...args:any[]) => g(f(args))
+
+
+const f1 = (x:number) => x + "f1!" ;
+
+const f2 = (x:string) => x + "f2!" ;
+
+
+console.log( compose(f1,f2)( 2 ) );
+console.log( compose2( compose2(f1,f2), f1 )( 2 ) );
+
+
+
+
+
+
+
+
+
+//console.log( fa( fn )(1) )
